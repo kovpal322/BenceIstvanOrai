5fa6aa046623ba0e96d236adc26e265186aea899
Neved <emailcimed@example.com>
2023-10-18 16:44:40 +0200

    egyeni html oldal

---
+++ Kovács_Pál_Roland/project-root/app/Views/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Default HTML Page</title>
+</head>
+
+<body>
+<header>
+    <h1>Welcome to My Website!</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section>
+        <h2>About Us</h2>
+        <p>This is a default HTML page. You can replace this content with your own.</p>
+    </section>
+
+    <section>
+        <h2>Services</h2>
+        <ul>
+            <li>Web Development</li>
+            <li>Graphic Design</li>
+            <li>Digital Marketing</li>
+        </ul>
+    </section>
+</main>
+
+<footer>
+    <p>&copy; 2023 My Website. All rights reserved.</p>
+</footer>
+</body>
+
+</html>
+
